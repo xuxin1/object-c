a648d7e407428a57d8fff892684bad6e55b311b8
xuxin1 <734532469@qq.com>
2020-12-01 10:15:22 +0800

    Update 选择排序.c

--- 选择排序.c
+++ 选择排序.c
@@ -1,45 +1,19 @@
-//Ñ¡ÔñÅÅĞò
-
-
-#include<stdio.h>
-int jiaohaun(int list[],int a,int b)
-{
-
-
-	int tmp;
-	tmp = list[a];
-	list[a]=list[b];
-	list[b]=tmp;
-	return list[10];
-}
-
-void print(int list[],int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
-
-	{
-		printf("%d ",list[i]);
-	}
-}
-
 int main()
 {	
 	int list[10]={3,4,2,4,2,4,2,6,8,63};
-	int n=10,j,i,tmp,preindex;
-	for (i = 1; i<n; i++)
-
-	{	
-		preindex=i-1;
-		tmp = list[i];
-		while (preindex>=0 && list[preindex]>tmp)
+	int n=10,j,i,minindex;
+	for (i = 0; i<n-1 ; i ++)    
+	{
+		minindex=i;
+		for (j = i+1; j<n; j++)
 		{
-			list[preindex+1]=list[preindex];
-			preindex--;
+			if (list[j]<list[minindex])
+			{
+				minindex=j;
+			}
 		}
-		list[preindex+1]=tmp;
-
+		jiaohaun(list,i,minindex);
+	
 	}
-	print(list,10);
+	print(list,n);
 }
-
