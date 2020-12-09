72ff41f507b805abd40cb0130694e33c4110951b
xuxin1 <734532469@qq.com>
2020-12-09 23:29:15 +0800

    Update 冒泡排序.c

--- 冒泡排序.c
+++ 冒泡排序.c
@@ -1,29 +1,26 @@
 //冒泡排序
 
-
 #include<stdio.h>
-
 int main()
 {	
 	int list[10]={3,4,2,4,2,4,2,6,8,63};
-
-	for (int i = 0; i < n; i++)
+	int n=10,i,j;
+	for ( i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n-1; j++)
+		for ( j = 0; j < n-1; j++)
 		{
 			if (list[j]>list[j+1])
 			{
-				int tmp=list[j]；
+				int tmp=list[j];
 				list[j]=list[j+1];
 				list[j+1]=tmp;
 
 			}
 		}
-		/* code */
 	}
 	for(i=0;i<n;i++)
 	{
 
-		printf("%d\n",list[i] );
+		printf("%d  ",list[i]);
 	}
 }
